9b0ad28737f31f8dd8ef6189f00b2b39097b80a9
Stoeffel <schtoeffel@gmail.com>
2015-10-22 08:38:50 +0200

    support react 0.14

--- example/src/index.js
+++ example/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import App from './App';
 
-React.render(<App />, document.getElementById('root'));
+ReactDom.render(<App />, document.getElementById('root'));
